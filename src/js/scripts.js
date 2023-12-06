a5254c5d2b03e79837c2a3b71acd8d8fb88ea014
jsunsjy <sjysun@bu.edu>
2023-12-05 23:56:11 -0500

    js-update

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,156 +7,156 @@
 // - You'll need to link this file to your HTML :)
 
 let imageIndex = 1;
-  const totalImages = 3;
+const totalImages = 3;
 
-  const displayedImage = document.getElementById('displayed-image');
+const displayedImage = document.getElementById('displayed-image');
 
-  function prevImage() {
-    if (imageIndex - 1 > 0) {
-      imageIndex = imageIndex - 1
-    } else if (imageIndex - 1 <= 0) {
-      imageIndex = 3
-    }
-    displayedImage.src = `/images/w${imageIndex}.png`;
-    logImageIndex();
+function prevImage() {
+  if (imageIndex - 1 > 0) {
+    imageIndex = imageIndex - 1
+  } else if (imageIndex - 1 <= 0) {
+    imageIndex = 3
+  }
+  displayedImage.src = `/images/w${imageIndex}.png`;
+  logImageIndex();
+}
+
+function nextImage() {
+  imageIndex = (imageIndex % totalImages) + 1;
+  displayedImage.src = `/images/w${imageIndex}.png`;
+
+  logImageIndex();
+}
+
+function logImageIndex() {
+  console.log(`search-image：${imageIndex}`);
+}
+
+var imageIndex1 = 1;
+function ok1() {
+  // get-element  
+  var element1 = document.getElementById("one");
+  // display-none  
+  element1.style.display = "none";
+  var element2 = document.getElementById("two");
+  // display-none  
+  element2.style.display = "block";
+  const displayedImage2 = document.getElementById('displayed-image2');
+  if (imageIndex == 1) {
+    displayedImage2.src = `/images/${imageIndex}${imageIndex1}.png`;
+  } else if (imageIndex == 2) {
+    displayedImage2.src = `/images/${imageIndex}${imageIndex1}.png`;
+  } else if (imageIndex == 3) {
+    displayedImage2.src = `/images/${imageIndex}${imageIndex1}.png`;
+  }
+}
+
+function prevImage1() {
+  const displayedImage2 = document.getElementById('displayed-image2');
+  if (imageIndex1 - 1 > 0) {
+    imageIndex1 = imageIndex1 - 1
+  } else if (imageIndex1 - 1 <= 0) {
+    imageIndex1 = 3
+  }
+  if (imageIndex == 1) {
+    displayedImage2.src = `/images/${imageIndex}${imageIndex1}.png`;
+  } else if (imageIndex == 2) {
+    displayedImage2.src = `/images/${imageIndex}${imageIndex1}.png`;
+  } else if (imageIndex == 3) {
+    displayedImage2.src = `/images/${imageIndex}${imageIndex1}.png`;
   }
 
-  function nextImage() {
-    imageIndex = (imageIndex % totalImages) + 1;
-    displayedImage.src = `/images/w${imageIndex}.png`;
+}
 
-    logImageIndex();
+function nextImage1() {
+  const displayedImage2 = document.getElementById('displayed-image2');
+  imageIndex1 = (imageIndex1 % totalImages) + 1;
+
+  if (imageIndex == 1) {
+    displayedImage2.src = `/images/${imageIndex}${imageIndex1}.png`;
+  } else if (imageIndex == 2) {
+    displayedImage2.src = `/images/${imageIndex}${imageIndex1}.png`;
+  } else if (imageIndex == 3) {
+    displayedImage2.src = `/images/${imageIndex}${imageIndex1}.png`;
   }
+}
 
-  function logImageIndex() {
-    console.log(`当前图片索引：${imageIndex}`);
+function back() {
+  // get-element  
+  var element1 = document.getElementById("one");
+  // display-none  
+  element1.style.display = "block";
+  var element2 = document.getElementById("two");
+  // display-none  
+  element2.style.display = "none";
+
+  var backgroundment = document.getElementsByClassName('background');
+  backgroundment[0].style.backgroundImage = "url('/images/1.jfif')";
+  // overlay-background-color
+  var overlays = document.getElementsByClassName('overlay');
+  for (var i = 0; i < overlays.length; i++) {
+    overlays[i].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
   }
+}
 
-  var imageIndex1 = 1;
-  function ok1() {
-    // 获取元素  
-    var element1 = document.getElementById("one");
-    // display none  
-    element1.style.display = "none";
-    var element2 = document.getElementById("two");
-    // 设置display属性为none  
-    element2.style.display = "block";
-    const displayedImage2 = document.getElementById('displayed-image2');
-    if (imageIndex == 1) {
-      displayedImage2.src = `/images/${imageIndex}${imageIndex1}.png`;
-    } else if (imageIndex == 2) {
-      displayedImage2.src = `/images/${imageIndex}${imageIndex1}.png`;
-    } else if (imageIndex == 3) {
-      displayedImage2.src = `/images/${imageIndex}${imageIndex1}.png`;
-    }
+function ok2() {
+  // body-image 
+  var backgroundment = document.getElementsByClassName('background');
+  backgroundment[0].style.backgroundImage = "url('/images/2.jfif')";
+
+  // overlay-color 
+  var overlays = document.getElementsByClassName('overlay');
+  for (var i = 0; i < overlays.length; i++) {
+    overlays[i].style.backgroundColor = 'rgb(163 163 163 / 47%)';
   }
+  // get-element 
+  var element2 = document.getElementById("two");
+  // display-none  
+  element2.style.display = "none";
+  var element3 = document.getElementById("three");
+  // display-none  
+  element3.style.display = "block";
 
-  function prevImage1() {
-    const displayedImage2 = document.getElementById('displayed-image2');
-    if (imageIndex1 - 1 > 0) {
-      imageIndex1 = imageIndex1 - 1
-    } else if (imageIndex1 - 1 <= 0) {
-      imageIndex1 = 3
-    }
-    if (imageIndex == 1) {
-      displayedImage2.src = `/images/${imageIndex}${imageIndex1}.png`;
-    } else if (imageIndex == 2) {
-      displayedImage2.src = `/images/${imageIndex}${imageIndex1}.png`;
-    } else if (imageIndex == 3) {
-      displayedImage2.src = `/images/${imageIndex}${imageIndex1}.png`;
-    }
-
+  const displayedImage3 = document.getElementById('displayed-image3');
+  if (imageIndex == 1) {
+    displayedImage3.src = `/images/${imageIndex}${imageIndex1}a.png`;
+  } else if (imageIndex == 2) {
+    displayedImage3.src = `/images/${imageIndex}${imageIndex1}a.png`;
+  } else if (imageIndex == 3) {
+    displayedImage3.src = `/images/${imageIndex}${imageIndex1}a.png`;
   }
+}
 
-  function nextImage1() {
-    const displayedImage2 = document.getElementById('displayed-image2');
-    imageIndex1 = (imageIndex1 % totalImages) + 1;
+function Reset() {
+  // get-confirmation-card  
+  // var result = confirm("confirm to reset?");  
+  document.getElementById("confirmationModal").style.display = "block";
 
-    if (imageIndex == 1) {
-      displayedImage2.src = `/images/${imageIndex}${imageIndex1}.png`;
-    } else if (imageIndex == 2) {
-      displayedImage2.src = `/images/${imageIndex}${imageIndex1}.png`;
-    } else if (imageIndex == 3) {
-      displayedImage2.src = `/images/${imageIndex}${imageIndex1}.png`;
-    }
+}
+
+function queConfirmation() {
+  location.reload();
+}
+function closeConfirmation() {
+  document.getElementById("confirmationModal").style.display = "none";
+}
+
+function order() {
+  document.getElementById("three").style.display = "none";
+  // body-image
+  var backgroundment = document.getElementsByClassName('background');
+  backgroundment[0].style.backgroundImage = "url('/images/thank.png')";
+
+  // overlay-color 
+  var overlays = document.getElementsByClassName('overlay');
+  for (var i = 0; i < overlays.length; i++) {
+    overlays[i].style.backgroundColor = 'rgb(0 0 0 / 0%)';
   }
+}
 
-  function back() {
-    // 获取元素  
-    var element1 = document.getElementById("one");
-    // 设置display属性为none  
-    element1.style.display = "block";
-    var element2 = document.getElementById("two");
-    // 设置display属性为none  
-    element2.style.display = "none";
+function nextImage() {
+  // your code here
+}
 
-    var backgroundment = document.getElementsByClassName('background');
-    backgroundment[0].style.backgroundImage = "url('/images/1.jfif')";
-    // 修改类名为"overlay"的元素的背景颜色  
-    var overlays = document.getElementsByClassName('overlay');
-    for (var i = 0; i < overlays.length; i++) {
-      overlays[i].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
-    }
-  }
-
-  function ok2() {
-    // 修改body的背景图片  
-    var backgroundment = document.getElementsByClassName('background');
-    backgroundment[0].style.backgroundImage = "url('/images/2.jfif')";
-
-    // 修改类名为"overlay"的元素的背景颜色  
-    var overlays = document.getElementsByClassName('overlay');
-    for (var i = 0; i < overlays.length; i++) {
-      overlays[i].style.backgroundColor = 'rgb(163 163 163 / 47%)';
-    }
-    // 获取元素  
-    var element2 = document.getElementById("two");
-    // 设置display属性为none  
-    element2.style.display = "none";
-    var element3 = document.getElementById("three");
-    // 设置display属性为none  
-    element3.style.display = "block";
-
-    const displayedImage3 = document.getElementById('displayed-image3');
-    if (imageIndex == 1) {
-      displayedImage3.src = `/images/${imageIndex}${imageIndex1}a.png`;
-    } else if (imageIndex == 2) {
-      displayedImage3.src = `/images/${imageIndex}${imageIndex1}a.png`;
-    } else if (imageIndex == 3) {
-      displayedImage3.src = `/images/${imageIndex}${imageIndex1}a.png`;
-    }
-  }
-
-  function Reset() {
-    // 弹出确认框  
-    // var result = confirm("您确定要继续吗？");  
-    document.getElementById("confirmationModal").style.display = "block";
-
-  }
-
-  function queConfirmation() {
-    location.reload();
-  }
-  function closeConfirmation() {
-    document.getElementById("confirmationModal").style.display = "none";
-  }
-
-  function order() {
-    document.getElementById("three").style.display = "none";
-    // 修改body的背景图片  
-    var backgroundment = document.getElementsByClassName('background');
-    backgroundment[0].style.backgroundImage = "url('/images/thank.png')";
-
-    // 修改类名为"overlay"的元素的背景颜色  
-    var overlays = document.getElementsByClassName('overlay');
-    for (var i = 0; i < overlays.length; i++) {
-      overlays[i].style.backgroundColor = 'rgb(0 0 0 / 0%)';
-    }
-  }
-
-  function nextImage() {
-    // your code here
-  }
-  
-  const nextBtn = document.querySelector('#nextBtn');
-  nextBtn.addEventListener('click', nextImage);
+const nextBtn = document.querySelector('#nextBtn');
+nextBtn.addEventListener('click', nextImage);
