ae16cea10cf88f54cf44ecd53cf25a8f3c02da74
mliukx <mliukx@bu.edu>
2023-12-11 22:19:07 -0500

    updated

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -10,6 +10,8 @@ let currentMugImageIndex = 1;
 const totalMugImages = 3; // Assuming you have 3 mug images
 
 const displayedMugImage = document.getElementById('displayed-image');
+const displayedMugImage2 = document.getElementById('displayed-image-2');
+const displayedMugImage3 = document.getElementById('displayed-image3');
 const previousMugButton = document.querySelector('.previous-image');
 const nextMugButton = document.querySelector('.next-image');
 
@@ -47,12 +49,64 @@ function changeColorImage(change) {
   }
 
   const colorName = colorNames[currentColorImageIndex - 1]; // Arrays are 0-indexed
-  displayedColorImage.src = `images/w1-${colorName}.png`;
+  displayedColorImage.src = `images/w${currentMugImageIndex}-${colorName}.png`;
 }
 
 // Event listeners for the color gallery buttons
 previousColorButton.addEventListener('click', () => changeColorImage(-1));
 nextColorButton.addEventListener('click', () => changeColorImage(1));
+
+
+const nextButton1 = document.querySelector('.looksgood-button1');
+const nextButton2 = document.querySelector('.looksgood-button2');
+const sectionOne = document.getElementById('one');
+const sectionTwo = document.getElementById('two');
+const sectionThree = document.getElementById('three');
+const sectionFour = document.getElementById('four');
+const backButton = document.querySelector('.back-button2');
+const orderNow = document.querySelector('.order-now-button');
+
+// Function to hide a section
+function hideSection(section) {
+  section.style.display = 'none';
+}
+
+// Function to show a section
+function showSection(section) {
+  section.style.display = 'block';
+}
+
+// Event listener for the first "Looks Good! Next!" button
+nextButton1.addEventListener('click', function () {
+  hideSection(sectionOne);
+  showSection(sectionTwo);
+  displayedMugImage2.src = `images/w${currentMugImageIndex}.png`;
+  console.log("log is ", displayedMugImage.src)
+});
+
+
+// Event listener for the second "Looks Good! Next" button
+nextButton2.addEventListener('click', function () {
+  hideSection(sectionTwo);
+  showSection(sectionThree);
+  displayedMugImage3.src = `images/w${currentMugImageIndex}.png`;
+  console.log("log is ", displayedMugImage.src)
+});
+
+backButton.addEventListener('click', function () {
+  hideSection(sectionTwo);
+  showSection(sectionOne);
+});
+
+orderNow.addEventListener('click', function () {
+  hideSection(sectionThree);
+  // Assuming that you want to show the thank you image and it's wrapped in a section with an ID
+  showSection(sectionFour); // You should have a 'sectionFour' defined for this to work
+  displayedthankYouImage.src = `images/thank.png`; // This line ensures the correct image is set
+});
+
+
+
 
 
 
@@ -75,178 +129,4 @@ nextColorButton.addEventListener('click', () => changeColorImage(1));
 //   preButton.addEventListener('click', prevImage);
 //   console.log(preButton)
 // }
-
-function nextImage() {
-  imageIndex = (imageIndex % totalImages) + 1;
-  displayedImage.src = `/images/w${imageIndex}.png`;
-  console.log("nextfunction")
-}
-
-// const nextButton =
-//   document.querySelector(".next-image");
-// console.log(nextButton)
-// if (nextButton !== "undefined") {
-//   nextButton.addEventListener('click', nextImage1);
-//   console.log(nextButton)
-// }
-
-const looksgoodButton1 =
-  document.querySelector(".looksgood-button1");
-console.log(looksgoodButton1)
-if (looksgoodButton1 !== "undefined") {
-  looksgoodButton1.addEventListener('click', ok1);
-  console.log(looksgoodButton1)
-}
-
-
-function logImageIndex() {
-  console.log(`search-image：${imageIndex}`);
-}
-
-var imageIndex1 = 1;
-function ok1() {
-  // get-element  
-  var element1 = document.getElementById("one");
-  // display-none  
-  element1.style.display = "none";
-  var element2 = document.getElementById("two");
-  // display-none  
-  element2.style.display = "block";
-  const displayedImage2 = document.getElementById('displayed-image2');
-  if (imageIndex == 1) {
-    displayedImage2.src = `/images/${imageIndex}${imageIndex1}.png`;
-  } else if (imageIndex == 2) {
-    displayedImage2.src = `/images/${imageIndex}${imageIndex1}.png`;
-  } else if (imageIndex == 3) {
-    displayedImage2.src = `/images/${imageIndex}${imageIndex1}.png`;
-  }
-}
-
-function prevImage1() {
-  const displayedImage2 = document.getElementById('displayed-image2');
-  if (imageIndex1 - 1 > 0) {
-    imageIndex1 = imageIndex1 - 1
-  } else if (imageIndex1 - 1 <= 0) {
-    imageIndex1 = 3
-  }
-  if (imageIndex == 1) {
-    displayedImage2.src = `/images/${imageIndex}${imageIndex1}.png`;
-  } else if (imageIndex == 2) {
-    displayedImage2.src = `/images/${imageIndex}${imageIndex1}.png`;
-  } else if (imageIndex == 3) {
-    displayedImage2.src = `/images/${imageIndex}${imageIndex1}.png`;
-  }
-
-}
-
-function nextImage1() {
-  const displayedImage2 = document.getElementById('displayed-image2');
-  imageIndex1 = (imageIndex1 % totalImages) + 1;
-
-  if (imageIndex == 1) {
-    displayedImage2.src = `/images/${imageIndex}${imageIndex1}.png`;
-  } else if (imageIndex == 2) {
-    displayedImage2.src = `/images/${imageIndex}${imageIndex1}.png`;
-  } else if (imageIndex == 3) {
-    displayedImage2.src = `/images/${imageIndex}${imageIndex1}.png`;
-  }
-}
-
-const nextColor =
-  document.querySelector(".next-image-color");
-console.log(nextColor)
-if (nextColor !== "undefined") {
-  nextbutton.addEventListener('click', nextImage1);
-  console.log(nextColor)
-}
-
-function back() {
-  // get-element  
-  var element1 = document.getElementById("one");
-  // display-none  
-  element1.style.display = "block";
-  var element2 = document.getElementById("two");
-  // display-none  
-  element2.style.display = "none";
-
-  var backgroundment = document.getElementsByClassName('background');
-  backgroundment[0].style.backgroundImage = "url('/images/1.jfif')";
-  // overlay-background-color
-  var overlays = document.getElementsByClassName('overlay');
-  for (var i = 0; i < overlays.length; i++) {
-    overlays[i].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
-  }
-}
-
-const backButton2 =
-  document.querySelector(".back-button2");
-console.log(backButton2);
-if (backButton2) {  // Checks if backButton2 is not null or undefined
-  backButton2.addEventListener('click', back);
-  console.log("Event listener added to backButton2");
-}
-
-function ok2() {
-  // body-image 
-  var backgroundment = document.getElementsByClassName('background');
-  backgroundment[0].style.backgroundImage = "url('/images/2.jfif')";
-
-  // overlay-color 
-  var overlays = document.getElementsByClassName('overlay');
-  for (var i = 0; i < overlays.length; i++) {
-    overlays[i].style.backgroundColor = 'rgb(163 163 163 / 47%)';
-  }
-  // get-element 
-  var element2 = document.getElementById("two");
-  // display-none  
-  element2.style.display = "none";
-  var element3 = document.getElementById("three");
-  // display-none  
-  element3.style.display = "block";
-
-  const displayedImage3 = document.getElementById('displayed-image3');
-  if (imageIndex == 1) {
-    displayedImage3.src = `/images/${imageIndex}${imageIndex1}a.png`;
-  } else if (imageIndex == 2) {
-    displayedImage3.src = `/images/${imageIndex}${imageIndex1}a.png`;
-  } else if (imageIndex == 3) {
-    displayedImage3.src = `/images/${imageIndex}${imageIndex1}a.png`;
-  }
-}
-const looksgoodButton2 =
-  document.querySelector(".looksgood-button2");
-console.log(looksgoodButton2)
-if (looksgoodButton2 !== "undefined") {
-  looksgoodButton1.addEventListener('click', ok2);
-  console.log(looksgoodButton2)
-}
-
-function Reset() {
-  // get-confirmation-card  
-  // var result = confirm("confirm to reset?");  
-  document.getElementById("confirmationModal").style.display = "block";
-
-}
-
-function queConfirmation() {
-  location.reload();
-}
-function closeConfirmation() {
-  document.getElementById("confirmationModal").style.display = "none";
-}
-
-function order() {
-  document.getElementById("three").style.display = "none";
-  // body-image
-  var backgroundment = document.getElementsByClassName('background');
-  backgroundment[0].style.backgroundImage = "url('/images/thank.png')";
-
-  // overlay-color 
-  var overlays = document.getElementsByClassName('overlay');
-  for (var i = 0; i < overlays.length; i++) {
-    overlays[i].style.backgroundColor = 'rgb(0 0 0 / 0%)';
-  }
-}
-
-
 
